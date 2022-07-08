1c16e8c1d02ca4d4de6feff3406708d2bc64097e
er-ebrahimi <erfan.es1381@gmail.com>
2022-07-08 21:02:53 +0430

    can calculate simple expretion but it has probleme with sin() or something like this

--- RPN.py
+++ RPN.py
@@ -1,8 +1,13 @@
 # from curses.ascii import isdigit
 # input('hello')
 # try:
-from typing import List
 
+from turtle import pos
+from typing import List
+import math
+import numpy
+from traitlets import Instance
+from numpy import log as ln
 
 operator = {'(': 1, ')': 1, '+': 2, '-': 2, '*': 3, '/': 3, '^': 4,
             'tan(': 1, 'cos(': 1, 'sin(': 1, 'ln(': 1, 'abs(': 1, 'exp(': 1}
@@ -31,8 +36,40 @@ for i in range(2, len(exper)):
         stack.append(exper[i])
 for i in range(len(stack)):
     postfix.append(stack.pop())
+print(postfix)
 num = []
-
-# except:
-#     print('reject')
-# 10 / 2 + 4 * -3.5
+for i in range(len(postfix)):
+    try:
+        if float(postfix[i]):
+            num.append(float(postfix[i]))
+            continue
+    except ValueError:
+        pass
+    if postfix[i] == '*':
+        temp = num.pop()
+        num.append(num.pop() * temp)
+    elif postfix[i] == '/':
+        temp = num.pop()
+        num.append(num.pop() / temp)
+    elif postfix[i] == '+':
+        temp = num.pop()
+        num.append(num.pop() + temp)
+    elif postfix[i] == '-':
+        temp = num.pop()
+        num.append(num.pop() - temp)
+    elif postfix[i] == 'sin(':
+        num.append(math.sin(num.pop()))
+    elif postfix[i] == 'cos(':
+        num.append(math.cos(num.pop()))
+    elif postfix[i] == 'tan(':
+        num.append(math.tan(num.pop()))
+    elif postfix[i] == 'ln(':
+        num.append(math.log(10, num.pop()))
+    elif postfix[i] == 'abs(':
+        num.append(abs(num.pop()))
+    elif postfix[i] == '^':
+        num.append(num.pop() ** num.pop())
+    elif postfix[i] == 'exp(':
+        num.pop(math.exp(num.pop()))
+format_float = "{:.2f}".format(num[0])
+print(format_float)
